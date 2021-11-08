8f40f9458db4d187544896cf48381117376ecfae
fercode-07 <66274999+fercode-07@users.noreply.github.com>
2021-11-08 00:12:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+gsap.from('.boton', {
+	
+	opacity: 0,
+	delay: 2.5,
+	stagger: 0.5,
+	duration: 1,
+	y: 50,
+	ease: 'elastic',
+	scale: 2,
+});
+
+gsap.from('.pic', {
+
+	opacity: 0,
+	delay: .4,
+	duration: 3,
+});
+
+
+
+
